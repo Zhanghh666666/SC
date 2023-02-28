9b1a7d7787c5111316672f5e56f41aef13122555
zhanghaha <68266633+Zhanghh666666@users.noreply.github.com>
2023-02-28 14:56:13 +0800

    Add files via upload

--- SC.cpp
+++ SC.cpp
@@ -1,4 +1,7 @@
 ﻿//执行txt中任意行的指令
+//2.27    mips 执行完成
+//2.28    更改 RISC-V的部分指令集，需要找到更好的ID方式，现在的方式形式不统一，且很复杂，是按op-funct3-funct7分类的，如果是按照六种类型分类也许更好
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -65,33 +68,79 @@ string ReadLine(string filename, int line){
 
 }
 
+
+//RISC-V 指令集译码
+//处理立即数时需要先拓展成32位
+
+//需要31-0处理，是正着读机器码
+
+//算数、移位、逻辑操作的op
+// 判定逻辑 op -> funct3 -> funct7
+//立即数op：0010011   
+//寄存器op：0110011
+
+//访存指令
+//立即数：0000011
+//寄存器：0100011
+
+//J type
+//1100011
+//1100111
+
+//csr
+//1110011
+
+
+
+
 //Instruction Decode  译码阶段
-//获得 op
-int ID_op(string Ins) {
-	string op = Ins.substr (0, 6);   // 前 6 bit 的 op
-	//cout << op << endl;
-	int num = stoi(op, 0, 2);
-	return num;
-}
 //获得指令中指定部分的结果，返回 int 
 int ID_get(string Ins, int start, int size) {
 	string r = Ins.substr(start, size);
 	int num = stoi(r, 0, 2);
 	return num;
 }
-获取指令中的指定部分，返回 string
+//获取指令中的指定部分，返回 string
 string ID_getString(string Ins, int start, int size) {
 	string r = Ins.substr(start, size);
 	return r;
 }
 
-//EX
+//EX   执行阶段
 int ADD(int r1, int r2) {
 	return r1 + r2;
 }
 int SUB(int r1, int r2) {
 	return r1 - r2;
 }
+
+int xori(int num, int imm) {
+	int result= imm ^ num;
+	return result;
+}
+int ori(int num, int imm){
+	int result = imm | num;
+	return result;
+}
+int andi(int num, int imm){
+	int result = imm & num;
+	return result;
+}
+int xor_(int num1, int num2){
+	int result = num1 ^ num2;
+	return result;
+}
+int or_(int num1, int num2){
+	int result = num1 | num2;
+	return result;
+}
+int and_(int num1, int num2){
+	int result = num1 & num2;
+	return result;
+}
+
+
+
 
 
 void main()
@@ -102,7 +151,11 @@ void main()
 	int pc = 1;
 	int R[32];   //模拟32个寄存器，需要根据标准mips库更改成4个int，4个floct等等功能寄存器
 
-	R[1] = R[2] = 10;   //假设初始化值
+	
+	for (int i = 0; i < 32; i++)
+		R[i] = i;
+	
+	//假设初始化值
 
 
 	while (stop) {
@@ -118,23 +171,177 @@ void main()
 
 
 	//译码，解析op，同时取寄存器中值,需要分出来三种指令,r，i和j，根据某一位的不同分别译码
-		string type = Ins.substr(0, 2);
-		int op = ID_op(Ins);
+	//执行，switch后直接执行，或者计算一个 6 bit + 多选器选择 bit 的译码结果，在后期统一执行，或许在乘法运算时需要
+		unsigned short int op = ID_get(Ins,25,7);
 				
-		//三个寄存器
-		int r_1;
-		int r_2;
-	    int r_3; 
+		//三个寄存器的号码
+		unsigned short int rs1;
+		unsigned short int rs2;
+		unsigned short int rd;
+
+		//两个funct 
+		unsigned short int funct_3, funct_7;
+
+		//立即数,所有指令最多一共4个立即数，从高位到低位数
+		unsigned short int imm1,imm2,imm3,imm4;
 
 
-		//两个操作数和中间结果
+
+		//两个取寄存器的操作数和 中间运算结果
 		int num_1 ;
 		int num_2 ;  
 		int num_result;
 
+		switch (op) {
+			case 19:                  //op=0010011       R型
+				funct_3 = ID_get(Ins, 17, 3);
+				switch (funct_3) {
+					case	0:		//funct3=000  addi
+						cout << "Instruction:addi" << endl;
+						break;
+					case	2:		//funct3=010  slti
+						cout << "Instruction:slti" << endl;
+						break;
+					case	3:		//funct3=011  sltiu
+						cout << "Instruction:sltiu" << endl;
+						break;
+					case	1:		//funct3=001  slli
+						cout << "Instruction:slli" << endl;
+						break;
+					case	5:		//funct3=101  srli/srai
+						funct_7 = ID_get(Ins, 0, 7);
+						if(funct_7==0)
+							cout << "Instruction:srli" << endl;
+						else
+							cout << "Instruction:srai" << endl;
+						break;
+					
+					case	4:		//funct3=100  xori
+						cout << "Instruction:xori" << endl;
+						imm1 = ID_get(Ins, 0, 12);
+						rs1 = ID_get(Ins, 12, 5);
+						rd = ID_get(Ins, 20, 5);
+						num_1 = R[rs1];
+						num_result = xori(num_1, imm1);
+						R[rd] = num_result;
+						cout <<"result=" << num_result << endl;
+						break;
+					case	6:		//funct3=110  ori
+						cout << "Instruction:ori" << endl;
+						imm1 = ID_get(Ins, 0, 12);
+						rs1 = ID_get(Ins, 12, 5);
+						rd = ID_get(Ins, 20, 5);
+						num_1 = R[rs1];
+						num_result = ori(num_1, imm1);
+						R[rd] = num_result;
+						cout << "result=" << num_result << endl;
+						break;
+					case	7:		//funct3=111  andi
+						cout << "Instruction:andi" << endl;
+						imm1 = ID_get(Ins, 0, 12);
+						rs1 = ID_get(Ins, 12, 5);
+						rd = ID_get(Ins, 20, 5);
+						num_1 = R[rs1];
+						num_result = andi(num_1, imm1);
+						R[rd] = num_result;
+						cout << "result=" << num_result << endl;
+						break;
+				
+				}
+				break;
+				 
+			case 51:					//OP=0110011     I型
+				funct_3 = ID_get(Ins, 17, 3);
+				switch (funct_3) {
+
+					case	0:		//funct3=000  add/sub
+						funct_7 = ID_get(Ins, 0, 7);
+						if (funct_7 == 0)
+							cout << "Instruction:add" << endl;
+						else
+							cout << "Instruction:sub" << endl;
+						break;
+
+					case	2:		//funct3=010  slt
+						cout << "Instruction:xori" << endl;
+						break;
+					case	3:		//funct3=011  sltu
+						cout << "Instruction:xori" << endl;
+						break;
+					case	1:		//funct3=001  sll
+						cout << "Instruction:sll" << endl;
+						break;
+					case	5:		//funct3=101  srl/sra
+						funct_7 = ID_get(Ins, 0, 7);
+						if (funct_7 == 0)
+							cout << "Instruction:srl" << endl;
+						else
+							cout << "Instruction:sra" << endl;
+						break;
+
+					case	4:		//funct3=100  xor
+						cout << "Instruction:xor" << endl;
+						rs1 = ID_get(Ins, 12, 5);
+						rs2 = ID_get(Ins, 7, 5);
+						rd = ID_get(Ins, 20, 5);
+						num_1 = R[rs1];
+						num_2 = R[rs2];
+						num_result = xor_ (num_1, num_2);
+						R[rd] = num_result;
+						cout << "result=" << num_result << endl;
+						break;
+					case	6:		//funct3=110  or
+						cout << "Instruction:or" << endl;
+						rs1 = ID_get(Ins, 12, 5);
+						rs2 = ID_get(Ins, 7, 5);
+						rd = ID_get(Ins, 20, 5);
+						num_1 = R[rs1];
+						num_2 = R[rs2];
+						num_result = or_ (num_1, num_2);
+						R[rd] = num_result;
+						cout << "result=" << num_result << endl;
+						break;
+					case	7:		//funct3=111  and
+						cout << "Instruction:and" << endl;
+						rs1 = ID_get(Ins, 12, 5);
+						rs2 = ID_get(Ins, 7, 5);
+						rd = ID_get(Ins, 20, 5);
+						num_1 = R[rs1];
+						num_2 = R[rs2];
+						num_result = and_ (num_1, num_2);
+						R[rd] = num_result;
+						cout << "result=" << num_result << endl;
+						break;
+				}
+				break;
 
 
-		if( type == "00")     //解析 R 型指令
+			case 3:					//op=0000011  load/store
+				cout << "Instruction:load/store" << endl;
+				break;
+			case 35:					//op=0100011  store    S型
+				cout << "Instruction:load/store" << endl;
+				break;
+			case 99:					//op=1100011  J      B型
+				cout << "Instruction:J type" << endl;
+				break;
+			case 103:				//op=1100111  J         J型
+				cout << "Instruction:J type" << endl;
+				break;
+			case 115:				//op=1110011   csr
+				cout << "Instruction:csre" << endl;
+				break;
+			default:
+				cout << "Please Check Your Instruction" << endl;
+				stop = false;
+		}
+
+
+
+
+
+		/*
+		//if( type == "00")     //解析 R 型指令
 		{
 			r_1 = ID_get(Ins,6,5);
 			r_2 = ID_get(Ins,11,5);
@@ -158,7 +365,7 @@ void main()
 		{
 			cout << "J型指令"<<endl;
 		}
-
+		
 
 	//EX   译码阶段已经识别好三种类型 现在直接根据 op 做计算就行了
 		switch (op) {
@@ -171,16 +378,16 @@ void main()
 
 
 		}
-
+*/
 		
 	//MEM  暂时没有
 
 
 
 
-	//WB  将中间结果写回寄存器
-		R[r_3] = num_result;
-		cout << "R3=" << R[r_3] << endl;
+	//WB  将中间结果写回寄存器，可以根据译码结果确定是否写会，暂时放在译码的switch中，紧接ex之后
+	//	R[r_3] = num_result;
+	//	cout << "R3=" << R[r_3] << endl;
 	
 	}
 
